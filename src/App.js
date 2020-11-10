76426a97051045966a29da3acefd9084b040fbfa
Walter Neto <wneto.macedo@gmail.com>
2020-11-09 20:38:48 -0600

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  // Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = ""; // Will hide the token in the URL
+    const token = hash.access_token;
+  }, []); // Anytime something in here changes it will run the code inside the useEffect
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Login />
     </div>
   );
 }
